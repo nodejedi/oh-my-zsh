4bc12ec6a897026624744ba647a3f41ce66e1c0d
Jon Kinney <jonkinney@gmail.com>
2011-05-06 00:22:59 -0500

    add j2fly shortcuts and update gitignore

---
+++ custom/j2fly_shortcuts.zsh
@@ -0,0 +1,7 @@
+alias proxyhome='ssh -D 2889 -C jkinney@studiomacpro.dyndns.org'
+alias proxysilver='ssh -D 2889 -C jkinney@studiomacpro.dyndns.org'
+
+alias tunnel_theda='ssh -L 3307:thedacareprod.cqgtmgitcxs6.us-east-1.rds.amazonaws.com:3306 theda-ec2'
+alias jbatv='cd ~/Desktop/tethered; ./tetheredboot -i iBSS.k66ap.RELEASE.dfu -k kernelcache.release.k66'
+
+alias uldb='sudo /usr/libexec/locate.updatedb' #update the location database
